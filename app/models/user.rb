a118eafac511c325384f3c1b5b8862c33217cefd
yucheng xia <yucheng.xia@ieseg.fr>
2024-04-03 18:24:54 +0200

    pages updated

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,18 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  # Validation for mandatory acceptance of privacy statement
+  validates :privacy_statement, acceptance: true
+
+
+  # Custom validation for password complexity
+  validate :password_complexity
+
+  private
+
+  def password_complexity
+    return unless password.present? && !password.match(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[\W\_]).{8,}$/)
+
+    errors.add :password, 'must include at least one lowercase letter, one uppercase letter, one digit, and one special character'
+  end
 end
