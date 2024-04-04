011ccef546a30b7aeba01348acbbf1fc897c8796
YUE PENG <yue.peng@ieseg.fr>
2024-04-04 14:18:02 +0200

    modify homepage

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   root to: "pages#home"
   get '/privacy', to: 'pages#privacy_statement', as: 'privacy_statement'
   get '/profile', to: 'users#profile', as: 'profile'
+  get '/home', to: 'users#home', as: 'afterloginhp'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
