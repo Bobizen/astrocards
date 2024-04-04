b1e9a115c096c36ebdc1e5fc0bfd85864aef949c
Olivier Perrin <olivier.perrin@ieseg.fr>
2024-04-04 20:24:19 +0200

    collection

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,10 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  after_commit :give_initial_cards, on: [:create]
+
   # Validation for mandatory acceptance of privacy statement
   validates :privacy_statement, acceptance: true
-
-
   # Custom validation for password complexity
   validate :password_complexity
 
@@ -14,7 +15,10 @@ class User < ApplicationRecord
 
   def password_complexity
     return unless password.present? && !password.match(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[\W\_]).{8,}$/)
-
     errors.add :password, 'must include at least one lowercase letter, one uppercase letter, one digit, and one special character'
+  end
+
+  def give_initial_cards
+    @user = current_user
   end
 end
